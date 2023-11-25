084d2aa78fc213eafad37dd8361697344755c9bc
mikhailyatsenko <misha.ggg@gmail.com>
2023-11-25 12:54:21 +0100

    remove all files before create next app

--- src/test/handlers.ts
+++
@@ -1,8 +0,0 @@
-import { http, HttpResponse } from 'msw';
-import { artsListMock } from './mocks/artsListMock';
-
-export const handlers = [
-  http.get('https://api.artic.edu/api/v1/artworks/*', () => {
-    return HttpResponse.json(artsListMock);
-  }),
-];
