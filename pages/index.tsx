040b214334bcdb9d3d00d02912518d7e370cae7a
mikhailyatsenko <misha.ggg@gmail.com>
2024-01-02 14:12:41 +0100

    add jest libraries, delete unuseful Layout.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,28 @@
-import Layout from './layout';
-import { DetailArt } from './layout';
 import { fetchAllArts, fetchSelectedArt } from '@/services/ArtService';
 import { wrapper } from '@/store';
 import { TransformedArtsListResponse } from '@/services/ArtService';
+
+import Pagination from '@/components/Pagination';
+import ListOfArts from '@/components/ListOfArts';
+import SelectedArt from '@/components/SelectedArt';
+
+import Head from 'next/head';
+import Search from '@/components/Search';
+import SelectItemsPerPage from '@/components/SelectItemsPerPage';
+
+export interface DetailArt {
+  artist_display: string;
+  title: string;
+  image_id: string;
+  provenance_text: string;
+}
+
+export type Arts = {
+  id: string;
+  artist_display: string;
+  title: string;
+  image_id: string;
+};
 
 interface SevereSideProps extends TransformedArtsListResponse {
   limit: string;
@@ -10,7 +30,26 @@ interface SevereSideProps extends TransformedArtsListResponse {
 }
 
 export default function Home(props: SevereSideProps) {
-  return <Layout {...props} />;
+  const { arts, detailArt, limit, totalPages } = props;
+  return (
+    <>
+      <Head>
+        <title>Arts App</title>
+        <meta name="description" content="App for load arts info" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Search />
+      <SelectItemsPerPage limit={limit} />
+      <main>
+        <div className="container-arts">
+          <ListOfArts arts={arts} />
+          {detailArt ? <SelectedArt detailArt={detailArt} /> : null}
+        </div>
+        {totalPages ? <Pagination totalPages={totalPages} /> : null}
+      </main>
+    </>
+  );
 }
 
 export const getServerSideProps = wrapper.getServerSideProps(
