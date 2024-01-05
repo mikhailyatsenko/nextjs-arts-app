8c063e3ea21745c10618933276177833b5ce3da1
mikhailyatsenko <misha.ggg@gmail.com>
2024-01-05 15:17:36 +0100

    rename test siut case

--- __tests__/Search.test.tsx
+++ __tests__/Search.test.tsx
@@ -7,19 +7,19 @@ import { fireEvent } from '@testing-library/react';
 jest.mock('next/router', () => jest.requireActual('next-router-mock'));
 
 describe('Home', () => {
-  //   test('Search input has a text from router query', async () => {
-  //     mockRouter.push('?search=test+search+query');
+  test('Search input has a text from router query', async () => {
+    mockRouter.push('?search=test+search+query');
 
-  //     render(<Search />);
-  //     const inputElement = screen.getByRole('textbox') as HTMLInputElement;
-  //     expect(inputElement.value).toBe('test search query');
-  //   });
+    render(<Search />);
+    const inputElement = screen.getByRole('textbox') as HTMLInputElement;
+    expect(inputElement.value).toBe('test search query');
+  });
 
   test('Submitting search value changes the value in query params', async () => {
     render(<Search />);
 
     const inputElement = screen.getByTestId('input-search') as HTMLInputElement;
-    // const button = screen.getByTestId('button-search');
+    const button = screen.getByTestId('button-search');
 
     fireEvent.change(inputElement, {
       target: { value: 'another test search query' },
@@ -27,8 +27,10 @@ describe('Home', () => {
 
     expect(inputElement.value).toBe('another test search query');
 
-    fireEvent.click(screen.getByRole('button'));
+    fireEvent.click(button);
 
-    expect(mockRouter.query.search).toBe('another test search query');
+    expect(mockRouter).toMatchObject({
+      query: { search: 'another test search query' },
+    });
   });
 });
